943dc5b03e9ba34ee48c3355cb60ca2d7d9d02ae
phoneixtaperlabs <phoenix@tape.ooo>
2023-09-25 15:03:41 +0900

    [ADD] Permission Hanlder 및 폴더 구저 확립 + 리팩토링

---
+++ macos/Classes/Microphone/MicrophoneRecorder+Temp.swift
@@ -0,0 +1,8 @@
+//
+//  MicrophoneRecorder+Temp.swift
+//  shadow
+//
+//  Created by Phoenix on 2023/09/21.
+//
+
+import Foundation
---
+++ macos/Classes/PermissionHandler.swift
@@ -0,0 +1,8 @@
+//
+//  PermissionHandler.swift
+//  shadow
+//
+//  Created by Phoenix on 2023/09/25.
+//
+
+import Foundation
