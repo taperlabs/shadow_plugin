3dbf3199255140ffc909a17d0736019afd5c5217
phoneixtaperlabs <phoenix@tape.ooo>
2024-01-08 14:49:36 +0900

    [add] App's AppSupport Directory 로직 추가

--- macos/Classes/Utils/FileManager/FileManagerHelper.swift
+++ macos/Classes/Utils/FileManager/FileManagerHelper.swift
@@ -17,15 +17,40 @@ extension FileManager.SearchPathDirectory {
 // MARK: - File Manager Helper 관리 구조체
 struct FileManagerHelper {
     
-    //set or get a file path
     static func getURL(for filename: String, in directoryString: String ) -> URL? {
         guard let directory = FileManager.SearchPathDirectory.from(string: directoryString) else {
             print("Invalid directory string: \(directoryString)")
             return nil
         }
         
-        return FileManager.default.urls(for: directory, in: .userDomainMask).first?.appendingPathComponent(filename)
+        guard let directoryURL = FileManager.default.urls(for: directory, in: .userDomainMask).first else {
+            print("Unable to find directory URL")
+            return nil
+        }
+        
+        // Append the app's bundle identifier to the path
+        let appSpecificDirectoryURL = directoryURL.appendingPathComponent(Bundle.main.bundleIdentifier ?? "com.taperlabs.shadow")
+
+        // Create the directory if it doesn't exist
+        do {
+            try FileManager.default.createDirectory(at: appSpecificDirectoryURL, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+            print("Failed to create directory: \(error)")
+            return nil
+        }
+
+        return appSpecificDirectoryURL.appendingPathComponent(filename)
     }
+    
+    //set or get a file path
+//    static func getURL(for filename: String, in directoryString: String ) -> URL? {
+//        guard let directory = FileManager.SearchPathDirectory.from(string: directoryString) else {
+//            print("Invalid directory string: \(directoryString)")
+//            return nil
+//        }
+//        
+//        return FileManager.default.urls(for: directory, in: .userDomainMask).first?.appendingPathComponent(filename)
+//    }
     
     //get url and delete file if existing
     static func deleteFileIfExists(at url: URL) {
