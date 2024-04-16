8c8a8bfc67b4e4a39a8e603d059ca949dca1a15e
phoneixtaperlabs <phoenix@tape.ooo>
2024-04-16 15:36:49 +0900

    [add] SC Bug Handler

---
+++ macos/Classes/ScreenCaptureKitBugHandler.swift
@@ -0,0 +1,71 @@
+import Foundation
+import ScreenCaptureKit
+import FlutterMacOS
+
+//MARK: SCBug Handler (Closure Bug)
+final class SCBugHandler: NSObject, FlutterStreamHandler {
+    private(set) var scAPICallCount: Int = 0
+    private let scAPICallCounterThreshold: Int = 2
+    private var windowCheckTimer: Timer?
+    private(set) var isSCError: Bool = false
+    private var eventSink: FlutterEventSink?
+    
+    func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        print("SCBug Handler Event On 🟢")
+        self.eventSink = events
+        return nil
+    }
+    
+    func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        self.resetAllVariables()
+        return nil
+    }
+    
+    private func resetAllVariables() {
+        self.scAPICallCount = 0
+        self.eventSink = nil
+        self.endWindowCheckTimer()
+        self.isSCError = false
+    }
+    
+    private func startWindowCheckTimer() {
+        windowCheckTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
+            self?.fetchWindows()
+        }
+    }
+    
+    private func endWindowCheckTimer() {
+        windowCheckTimer?.invalidate()
+        windowCheckTimer = nil
+    }
+    
+    private func checkAPIResponse() {
+        if self.scAPICallCount > self.scAPICallCounterThreshold {
+            print("API did not respond in time, taking corrective action...")
+            self.isSCError = true
+        }
+    }
+    
+    private func fetchWindows() -> Void {
+        print("Fetch Window for ScreenRecording Permission")
+        self.scAPICallCount += 1
+        DispatchQueue.global().async { [weak self] in
+            SCShareableContent.getExcludingDesktopWindows(false, onScreenWindowsOnly: false) { content, error in
+                print("Fetch Window for ScreenRecording Permission inside a closure")
+                if let error = error {
+                    self?.scAPICallCount = 0
+                    print(error.localizedDescription)
+                }
+                guard let content = content else { return }
+                print("Fetch Window for ScreenRecording Permission inside a closure 2")
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    self?.scAPICallCount = 0
+                }
+            }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.checkAPIResponse()
+        }
+    }
+}
